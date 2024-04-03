584a213e735c05090e358367289656b6ed28c0d5
mathiasmantai <mmantaibusiness@gmail.com>
2024-04-03 14:39:15 +0200

    style: created icons for the app

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sys import platform
 if __name__ == '__main__':
     config_file_path = path.join(path.dirname(__file__), 'src', 'data')
     config_file_name = 'config.json'
-    icon_file_path = path.join(path.dirname(__file__), 'src', 'assets', 'icon.png')
+    icon_file_path = path.join(path.dirname(__file__), 'src', 'assets', 'icon_32x32.png')
     log_file_path = path.join(path.dirname(__file__), 'src', 'log')
     app = App('Desktop Organizer', config_file_path, config_file_name, log_file_path, icon_file_path, 990, 450)
     app.build_ui()
